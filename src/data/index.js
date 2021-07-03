78a4a1d6a3484b3705b47fde427680e040da69ae
vuduc <vxduc9999@gmail.com>
2021-07-03 09:56:12 +0700

    Setup NanoId I use NanoId to generate dynamic ids for lists and tasks
    References: RM-1002

--- src/data/index.js
+++ src/data/index.js
@@ -1,25 +1,34 @@
 const lists = [
   {
-    id: 1,
+    id: "xzN_2SRsBg-ijAGsUBI9h",
     name: "list 1",
   },
   {
-    id: 2,
-    name: "list 2",
+    id: "y3KgqeuBJdYwLVBI9aDbF",
+    name:
+      "list 2jhawbdjkawndjknakwjdnkjawnkjdnakjwndknawkjndkjanwdkjnawkjndkjnawkjdnwkandkawndkjwankdnawk akjnw kjdnakjw n",
   },
 ];
 
 const tasks = [
   {
-    id: 0,
-    id_list: 1,
-    content: "Di hoc",
+    id: 1,
+    id_list: "xzN_2SRsBg-ijAGsUBI9h",
+    content:
+      "Di hoc kjanwj dkj anwdk ajwn daw wandkj nawjk awndknawkkj anwdjnaw kjanwdkjanwjkd kjanwdkjnawkj jkanwdkjnawk jkawndkjawawjnd kjawjkdnajk wdjkanw jnadkj ajw ndjkna wjn anajw ndjnaw ndanwk ndkjawn knawkj dnkjawndk akjwdn kjanwdnawndkjawjh abwhjdbjhaw habwhjdbjhaw jhabwdjhbawjhhjdab jhawbjhd bhjawbdjh bajwhdb jhabwj hdbjahwb jhwndkjnawkjdnkjawndjkwankjdnawkjndk",
     done: false,
-    priority: "low",
+    priority: "medium",
+  },
+  {
+    id: 2,
+    id_list: "xzN_2SRsBg-ijAGsUBI9h",
+    content: "Di hoc1",
+    done: false,
+    priority: "medium",
   },
   {
     id: 1,
-    id_list: 2,
+    id_list: "y3KgqeuBJdYwLVBI9aDbF",
     content: "Di lam",
     done: true,
     priority: "medium",
