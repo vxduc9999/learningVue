ec5db1f69ee40a47efcd88a8866c3843231c977d
vuduc <vxduc9999@gmail.com>
2021-07-09 14:09:56 +0700

    Modify add task button and list summary
    I updated the UI for the add list button to be better.
    
    In the list summary I take the list name and count all the tasks
    to show and update UI
    
    References: RM-1003

--- src/components/ListDetails/AddTaskButton.vue
+++ src/components/ListDetails/AddTaskButton.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class="add-task-button">
-    <button>AddTaskButton</button>
-  </div>
+  <button class="add-task-button">
+    <i class="material-icons">
+      add
+    </i>
+  </button>
 </template>
 
 <style scoped>
 .add-task-button {
   text-align: end;
+  width: 100%;
+  height: 3%;
+  border: none;
+  background: none;
 }
 </style>
--- src/components/ListDetails/ListSummary.vue
+++ src/components/ListDetails/ListSummary.vue
@@ -1,13 +1,42 @@
 <template>
   <div class="list-summary">
-    <h2>List Name</h2>
-    <h2>CountTaskOfList</h2>
+    <h2 class="list-name">{{ listName }}</h2>
+    <h2>{{ countAllTasks }}</h2>
   </div>
 </template>
+
+<script>
+export default {
+  name: "ListSummary",
+  props: {
+    listName: {
+      type: String,
+    },
+    countAllTasks: {
+      type: Number,
+    },
+  },
+};
+</script>
 
 <style scoped>
 .list-summary {
   display: flex;
   justify-content: space-between;
+  height: 50px;
+  margin-top: 15px;
+  padding-right: 10px;
+}
+
+h2 {
+  font-size: 40px;
+  color: blue;
+}
+
+.list-name {
+  width: 90%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 </style>
